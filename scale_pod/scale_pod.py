cfec424555147a1d8be1741e4473376c8048811d
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2024-12-05 19:53:01 +0200

    changes to all files_2

--- scale_pod/scale_pod.py
+++ scale_pod/scale_pod.py
@@ -28,25 +28,24 @@ def load_k8s_config():
 # Create the scale deployment function
 def scale_deployment(prefix: str, namespace: str, replicas: int):
     load_k8s_config()
-    
-
-    k8s_apps_v1 = client.AppsV1Api()
+    apps_v1 = client.AppsV1Api()
     
     # Specify the deployment name (using prefix)
     deployment_name = f"{prefix}-deployment"
+    try:
+            # Make the API call to update the deployment replicas
+            api_response = apps_v1.patch_namespaced_deployment_scale(
+                name=deployment_name, 
+                namespace=ScaleRequest.namespace, 
+                body={'spec': {'replicas': ScaleRequest.replicas}}
+    )
+            return {"message": f"Scaled {deployment_name} to {replicas} replicas"}
+    except ApiException as e:
+        raise HTTPException(status_code=400, detail=f"Error scaling deployment: {str(e)}")
     '''
     return deployment_name, namespace, replicas
 
-    try:
-        # Make the API call to update the deployment replicas
-        api_response = k8s_apps_v1.patch_namespaced_deployment_scale(
-            name=deployment_name, 
-            namespace=ScaleRequest.namespace, 
-            body={'spec': {'replicas': ScaleRequest.replicas}}
-)
-        return {"message": f"Scaled {deployment_name} to {replicas} replicas"}
-    except ApiException as e:
-        raise HTTPException(status_code=400, detail=f"Error scaling deployment: {str(e)}")
+    
     '''
 # Define the POST endpoint to scale deployments
     return deployment_name
