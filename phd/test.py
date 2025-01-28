5f41a0669b206d9d9935dc79cb22b2604bdee1ab
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2025-01-28 17:41:33 +0200

    changes to code

--- phd/test.py
+++ phd/test.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
         # Make a Scrape for the whole cluster
         try:
             cluster_sample = gather_metrics_for_15_seconds(NODE_NAME)
-            Gatherer.flush_data()
+            print(cluster_sample)
+            #Gatherer.flush_data()
         except Exception as err:
             print(f"Unexpected Collection Error (But DP Gathering again in {SCRAPE_INTERVAL} seconds). Error: {err=}, {type(err)=}")
             time.sleep(SCRAPE_INTERVAL)
