e5f7c4c97dc21c2ac15643db37c0118f428b189f
Pepi Paraskevoulakou <e.paraskevoulakou@unipi.gr>
2025-01-31 12:20:04 +0200

    changes to code

--- phd/test.py
+++ phd/test.py
@@ -11,6 +11,9 @@ SCRAPE_INTERVAL = int(os.getenv("SCRAPE_INTERVAL", 1)) # In Seconds
 # Main function to start the processes
 if __name__ == "__main__":
 
+    print({"data": "values"})
+    time.sleep(10)
+'''
     Gatherer.start_thread()
 
     while True:
@@ -27,4 +30,4 @@ if __name__ == "__main__":
 
         time.sleep(SCRAPE_INTERVAL)
 
-
+'''
